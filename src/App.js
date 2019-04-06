85c3597c97888a15b2fb085917a961d83a268ddb
said shah <saidmysr20@aol.com>
2019-04-05 21:53:36 -0400

    added calculator background and screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import Screen from './components/Screen'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="outerbody">
+        <div className="calculatorBody">
+
+          <Screen/>
+        </div>
 
       </div>
     );
---
+++ src/components/Screen.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Screen extends Component {
+
+  render() {
+    return (
+      <div className="whole-screen">
+        
+
+      </div>
+    );
+  }
+
+}
+
+export default Screen;
