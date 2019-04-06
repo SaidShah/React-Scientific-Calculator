5cdcef3c1986d3d5faaefb87ea0e12dec58e2474
said shah <saidmysr20@aol.com>
2019-04-06 18:58:03 -0400

    finished pemdas calculations

--- src/doMath.js
+++ src/doMath.js
@@ -1,7 +1,34 @@
-function doTheMath(givenItem){
+/* eslint no-eval: 0 */
+
+function doTheMath(givenItems){
+  let eqArray=givenItems.split("\n")
+  let index = eqArray.length-1
+  let givenItem = eqArray[index]
  if(checkParens(givenItem)=== -1){
-    document.getElementById("calcScreen").value+="\n Equation is Invalid"
+    document.getElementById("calcScreen").value+="\n Equation is Invalid\n"
  }else{
+
+   if((givenItem.indexOf("(")>=0)  && (givenItem.indexOf(")")>=1) ){
+     let newExp = givenItem.slice(0,givenItem.indexOf("("))+"*"+givenItem.slice(givenItem.indexOf("("),givenItem.length)
+     let newExp2 = newExp.slice(0,newExp.indexOf(")")+1)+"*"+newExp.slice(newExp.indexOf(")")+1,newExp.length)
+     if(newExp2.charAt(0)==="*"){
+       newExp2 = newExp2.replace("*","")
+     }if(newExp2.charAt(newExp2.length-1) === "*"){
+       newExp2 = newExp2.slice(0,-1)
+     }
+     
+     let answer = eval(newExp2)
+     document.getElementById("calcScreen").value+="\n"
+     document.getElementById("calcScreen").value+=answer
+     document.getElementById("calcScreen").value+="\n"
+
+   }else{
+     let answer = eval(givenItem)
+     document.getElementById("calcScreen").value+="\n"
+     document.getElementById("calcScreen").value+=answer
+     document.getElementById("calcScreen").value+="\n"
+
+   }
 
  }
 
@@ -10,7 +37,7 @@ function doTheMath(givenItem){
 function checkParens(givenItem){
   let leftParen = givenItem.indexOf("(")
   let rightParen = givenItem.indexOf(")")
-  if((leftParen === -1 && rightParen >0) || (rightParen === -1 && leftParen >0)){
+  if((leftParen === -1 && rightParen >=0) || (rightParen === -1 && leftParen >=0)){
     return -1
   }else {
     let counter = 0
